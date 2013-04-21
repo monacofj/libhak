426e765709eb2fad34c30f976bdd34e4a708d4c2
Monaco F J <monaco.fj@gmail.com>
2013-04-21 19:12:28 -0300

    Added sysfun

---
+++ lib/sysfun.c
@@ -0,0 +1,34 @@
+/*  sysfun.c - 
+    
+    Copyright 2013  Francisco Jose Monaco   <monaco.fj@gmail.com>
+
+    This file is part of Libhak. 
+
+    Libhak is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Libhak is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with .  If not, see <http://www.gnu.org/licenses/>. 
+
+*/
+
+#include <libhak.h>
+#include <stdlib.h>
+#include <libhak/sysfun.h>
+
+int hak_malloc(size_t size)
+{
+  void *block;
+  block = malloc (size);
+  hak_syserror (!size);
+  return size;
+}
+
+
